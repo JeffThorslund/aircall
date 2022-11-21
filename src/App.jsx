398cb9b2d75795d9a9de62684700b44d558a26b7
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-11-21 14:56:42 -0500

    run prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,62 @@
-import React, {useEffect, useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 
-import Header from './Header.jsx';
-import {archiveCallById, getAllCalls} from "./requests";
+import Header from "./Header.jsx";
+import { archiveCallById, getAllCalls } from "./requests";
 
 const App = () => {
-    const [calls, setCalls] = useState([]);
+  const [calls, setCalls] = useState([]);
 
-    useEffect(() => {
-        getAllCalls().then(calls => setCalls(calls))
-    }, []);
+  useEffect(() => {
+    getAllCalls().then((calls) => setCalls(calls));
+  }, []);
 
-
-    return (<div className='container'>
-        <Header/>
-        <div className="container-view">Some activities should be here</div>
-        <div>
-            Activity
-            {calls.filter(c => !c.is_archived).map(c => <div onClick={() => {
+  return (
+    <div className="container">
+      <Header />
+      <div className="container-view">Some activities should be here</div>
+      <div>
+        Activity
+        {calls
+          .filter((c) => !c.is_archived)
+          .map((c) => (
+            <div
+              onClick={() => {
                 archiveCallById(c.id, true).then(() => {
-                    getAllCalls().then(calls => setCalls(calls))
-                })
-            }} className={'item'} key={c.id}>{c.from}</div>)}
-        </div>
+                  getAllCalls().then((calls) => setCalls(calls));
+                });
+              }}
+              className={"item"}
+              key={c.id}
+            >
+              {c.from}
+            </div>
+          ))}
+      </div>
 
-        <div>
-            Archived
-            {calls.filter(c => c.is_archived).map(c => <div className={'item'}
-                                                            onClick={() => archiveCallById(c.id, false).then(() => {
-                                                                getAllCalls().then(calls => setCalls(calls))
-                                                            })} style={{color: 'red'}}
-                                                            key={c.id}>{c.from}</div>)}
-        </div>
-    </div>);
+      <div>
+        Archived
+        {calls
+          .filter((c) => c.is_archived)
+          .map((c) => (
+            <div
+              className={"item"}
+              onClick={() =>
+                archiveCallById(c.id, false).then(() => {
+                  getAllCalls().then((calls) => setCalls(calls));
+                })
+              }
+              style={{ color: "red" }}
+              key={c.id}
+            >
+              {c.from}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
