2eda08c9ead5fb23caa3c8a3700e164d73519f0b
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2022-11-21 13:53:40 -0500

    create api endpoint requests

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './Header.jsx';
+import {getCallById, getAllCalls, archiveCallById, resetCallsToInitialState} from "./requests";
 
 const App = () => {
+
+    useEffect( () => {
+        getAllCalls()
+        getCallById(7834)
+        archiveCallById(7834)
+        getCallById(7834)
+        resetCallsToInitialState()
+    }, []);
+
+
   return (
     <div className='container'>
       <Header/>
